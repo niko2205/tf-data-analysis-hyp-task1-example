3a4c9145b8bdaddd1ab40a41bac3960669c82ded
niko2205 <87126305+niko2205@users.noreply.github.com>
2023-04-13 22:03:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,14 +5,14 @@ from scipy.stats import norm
 
 chat_id = 252623629
 
-def solution(x_success: int,
-             x_cnt: int,
-             y_success: int,
-             y_cnt: int) -> bool:
-  control_conv = x_success / x_cnt
-  test_conv = y_success / y_cnt
-  dev = test_conv - control_conv
-  se = np.sqrt((test_conv*(1-test_conv)/y_cnt)+(control_conv*(1-control_conv)/x_cnt))
-  z_score = dev / se
-  p_val = 2*(1-norm.cdf(abs(z_score)))
-  return p_val < 0.03
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool: 
+  p1 = x_success / x_cnt 
+  p2 = y_success / y_cnt 
+  p = (x_success + y_success) / (x_cnt + y_cnt) 
+  sigma = np.sqrt(p*(1-p)*(1/x_cnt+1/y_cnt)) 
+  z = (p1 - p2) / sigma 
+  p_value = 2 * (1 - norm.cdf(abs(z))) 
+  return p_value < 0.03
