e9766c4a54d41200b40e80cb0442dc6874ca5648
niko2205 <87126305+niko2205@users.noreply.github.com>
2023-04-13 21:15:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,4 @@ def solution(x_success: int,
   se = np.sqrt((test_conv*(1-test_conv)/y_cnt)+(control_conv*(1-control_conv)/x_cnt))
   z_score = dev / se
   p_val = 2*(1-norm.cdf(abs(z_score)))
-  if p_val < 0.03:
-  return True
-  else:
-  return False
+  return p_val < 0.03
